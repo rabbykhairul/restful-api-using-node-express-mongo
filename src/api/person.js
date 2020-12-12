c6a03f7f3273d5d8b74deec0422c9959034ac56e
rabbykhairul <rabbykhairul@gmail.com>
2020-12-12 16:52:13 +0600

    add custom error handlers

--- src/api/person.js
+++ src/api/person.js
@@ -7,6 +7,10 @@ router.get("/", (req, res) => {
   else res.send("You have requested a person!!!");
 });
 
+router.get("/unknown", (req, res) => {
+  throw new Error("This is a forced error!");
+});
+
 router.get("/:name", (req, res) => {
   const personName = req.params.name;
   res.send(`You have requested a person who is named as ${personName}`);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 app.use((req, res, next) => {
-  console.log(`${Date.now().toString()} => ${req.originalUrl}`);
+  console.log(`${new Date().toString()} => ${req.originalUrl}`);
   next();
 });
 
 app.use(express.static("public"));
 app.use("/api/person", require("./api/person"));
+
+// Handler for unregistered routes - 404 error
+app.use((req, res, next) => {
+  res.status(404).send("We think you are lost!");
+});
+
+// Handler for internal error - 500 error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).sendFile(path.join(__dirname, "../public/500.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
