a4b926390301bf6331b232f0d45cc7246af6c7d3
rabbykhairul <rabbykhairul@gmail.com>
2020-12-12 16:23:32 +0600

    log basic info about all incoming requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`${Date.now().toString()} => ${req.originalUrl}`);
+  next();
+});
+
 app.use(express.static("public"));
-
-const PORT = process.env.PORT || 3000;
-
 app.use("/api/person", require("./api/person"));
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
