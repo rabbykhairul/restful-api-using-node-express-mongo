16ca9a67438134d9cc1119c880708a134e497a1a
rabbykhairul <rabbykhairul@gmail.com>
2020-12-12 15:11:24 +0600

    setup basic express node js server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
