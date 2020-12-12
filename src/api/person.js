4448bfa19a9e273af6d36d628541715742c58079
rabbykhairul <rabbykhairul@gmail.com>
2020-12-12 15:54:38 +0600

    define route handler for '/api/person/:name' endpoint

--- src/api/person.js
+++ src/api/person.js
@@ -5,4 +5,9 @@ router.get("/", (req, res) => {
   res.send("You have requested a person!!!");
 });
 
+router.get("/:name", (req, res) => {
+  const personName = req.params.name;
+  res.send(`You have requested a person who is named as ${personName}`);
+});
+
 module.exports = router;
