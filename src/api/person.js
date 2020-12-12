3a7dcdf5c85585a6f31210e04e670bcf73e519c9
rabbykhairul <rabbykhairul@gmail.com>
2020-12-12 16:06:45 +0600

    accept query string on '/api/person' endpoint

--- src/api/person.js
+++ src/api/person.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("You have requested a person!!!");
+  if (req.query.name)
+    res.send(`You have requested a person who is named as ${req.query.name}`);
+  else res.send("You have requested a person!!!");
 });
 
 router.get("/:name", (req, res) => {
